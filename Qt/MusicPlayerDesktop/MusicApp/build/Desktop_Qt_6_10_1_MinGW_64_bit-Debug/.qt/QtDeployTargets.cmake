c9d22a5234afa82911d951cd3c437036b34a2cb1
Kien Phan <quockien306@gmail.com>
2026-05-02 21:41:36 +0700

    clean

--- Qt/MusicPlayerDesktop/MusicApp/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/.qt/QtDeployTargets.cmake
+++
@@ -1,3 +0,0 @@
-set(__QT_DEPLOY_TARGET_appMusicApp_FILE D:/MyData/QT/MusicPlayerDesktop/MusicApp/build/Desktop_Qt_6_10_1_MinGW_64_bit-Debug/appMusicApp.exe)
-set(__QT_DEPLOY_TARGET_appMusicApp_TYPE EXECUTABLE)
-set(__QT_DEPLOY_TARGET_appMusicApp_RUNTIME_DLLS C:/Qt/6.10.1/mingw_64/bin/Qt6Quick.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6OpenGL.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Gui.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlMeta.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlModels.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6QmlWorkerScript.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Qml.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Network.dll;C:/Qt/6.10.1/mingw_64/bin/Qt6Core.dll)
